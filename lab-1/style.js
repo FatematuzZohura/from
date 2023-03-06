4640110aded816d171ff987c6bacb64e18cd6996
FatematuzZohura <85582509+FatematuzZohura@users.noreply.github.com>
2023-03-07 02:49:16 +0600

    Add files via upload

---
+++ lab-1/style.js
@@ -0,0 +1,22 @@
+document.getElementById("sign-up").addEventListener("click", function() {
+    const nameField = document.getElementById("namein");
+    const userName = nameField.value;
+    //console.log(userName);
+
+    const emailField = document.getElementById("email");
+    const userEmail = emailField.value;
+    //console.log(userEmail);
+
+    const passwordField = document.getElementById("password");
+    const userPassword = passwordField.value;
+    //console.log(userPassword);
+
+
+    //console.log(changePassword);
+
+    if (userEmail == "Fatema@gmail.com" && userPassword == "Fatema#") {
+        window.alert("Hello! sucessfull");
+    } else {
+        window.location.href = "index2.html";
+    }
+})
